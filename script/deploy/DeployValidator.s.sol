0d34ab02cbe5a3276b3a1d784ed2f9c995d241ab
Anton <golvcenkoanton@gmail.com>
2025-01-27 02:41:54 +0100

    Upd `Validator` deploy script

--- script/deploy/DeployValidator.s.sol
+++ script/deploy/DeployValidator.s.sol
@@ -3,19 +3,30 @@ pragma solidity 0.8.28;
 
 import {Script, console} from "forge-std/Script.sol";
 import {Validator} from  "../../src/Validator.sol";
+import {HelperConfig} from "../HelperConfig.s.sol";
+
 
 contract DeployValidator is Script {
     function run() public {
         deploy();
     }
 
-    function deploy() public returns (Validator) {
-        vm.startBroadcast();
-        Validator token = new Validator();
+    function deploy() public returns (Validator, HelperConfig.NetworkConfig memory) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfigByChainId(block.chainid);
+        
+        vm.startBroadcast(config.adminPk);
+        Validator validator = new Validator(
+            config.epochDuration,
+            config.rewardDecayRate,
+            config.initialRewards,
+            config.licenseToken,
+            config.rewardToken
+        );
         vm.stopBroadcast();
 
-        console.log("Validator contract deployed at: ", address(token));
+        console.log("Validator contract deployed at: ", address(validator));
 
-        return token;
+        return (validator, config);
     }
 }
